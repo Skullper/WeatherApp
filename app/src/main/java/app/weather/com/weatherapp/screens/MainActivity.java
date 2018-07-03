ba1d3e4ec1a4aca535606e22ed44463a31b2bd33
Roman Pavliuk <sckalper@gmail.com>
2018-07-03 09:57:30 +0300

    Initial commit

---
+++ app/src/main/java/app/weather/com/weatherapp/screens/MainActivity.java
@@ -0,0 +1,15 @@
+package app.weather.com.weatherapp.screens;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import app.weather.com.weatherapp.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/app/weather/com/weatherapp/screens/SplashActivity.java
@@ -0,0 +1,22 @@
+package app.weather.com.weatherapp.screens;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by pugman on 03.07.18.
+ * Contact the developer - sckalper@gmail.com
+ * company - A2Lab
+ */
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        startActivity(new Intent(this, MainActivity.class));
+        finish();
+    }
+}
